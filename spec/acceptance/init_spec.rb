31a3b5ba370c49f1f13bf40883afcfd9cfa84360
Mike Marseglia <mike@marseglia.org>
2018-05-24 12:19:36 -0400

    change to a different vagrant box because puppet's wasn't downloading

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -1,7 +1,7 @@
 # run a test task
 require 'spec_helper_acceptance'
 
-describe 'perforce is idempotent' do
+describe 'install and configure perforce' do
 
   let(:pp) do
     <<-MANIFEST
@@ -10,4 +10,14 @@ describe 'perforce is idempotent' do
   end
 
   it_behaves_like 'an idempotent resource'
+
+  it 'should have packages installed' do
+    rpm_results = shell("rpm -qa | grep helix-p4d")
+    expect(rpm_results.exit_code).to eq 0
+  end
+
+  describe service('p4d') do
+    it { should be_enabled }
+    it { should be_running }
+  end
 end
