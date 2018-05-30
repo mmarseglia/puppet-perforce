4bdd53a175b284c6a43f45f339d32a21bc837965
Mike Marseglia <mike@marseglia.org>
2018-05-30 17:09:09 -0400

    perforce can optionally be run using ssl. you must supply an absolute path as the service_ssldir parameter. the directory must exist and contain the privatekey.txt and certificate.txt files perforce requires.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,24 +1,39 @@
 require "#{File.join(File.dirname(__FILE__),'..','spec_helper')}"
 
 describe 'perforce' do
-  it { should compile }
 
-  it { should contain_class('perforce::install')}
-  it { should contain_class('perforce::configure')}
-  it { should contain_class('perforce::package')}
-  it { should contain_class('perforce::repository')}
-  it { should contain_class('perforce::service')}
+  context 'default parameters' do
+    it { should compile }
 
-  # perforce's main yum repository
-  it { should contain_yumrepo('perforce') }
+    it { should contain_class('perforce::install')}
+    it { should contain_class('perforce::configure')}
+    it { should contain_class('perforce::package')}
+    it { should contain_class('perforce::repository')}
+    it { should contain_class('perforce::service')}
 
-  # package installation
-  it { should contain_package('helix-p4d') }
-  it { should contain_package('helix-cli') }
+    # perforce's main yum repository
+    it { should contain_yumrepo('perforce') }
 
-  # perforce p4d service
-  it { should contain_service('p4d') }
+    # package installation
+    it { should contain_package('helix-p4d') }
+    it { should contain_package('helix-cli') }
 
-  # perforce p4d environment variables, configuration
-  it { should contain_file('/opt/perforce/.p4config') }
+    # perforce p4d service
+    it { should contain_service('p4d') }
+
+    # perforce p4d environment variables, configuration
+    it { should contain_file('/opt/perforce/.p4config') }
+  end
+
+  context 'with ssl' do
+    let (:params) {{
+        'service_ssldir'   => '/opt/perforce/p4ssldir',
+    }}
+
+    # perforce ssl directory environment variable is set
+    it { is_expected.to contain_file('/opt/perforce/.p4config').with_content(/^P4SSLDIR/) }
+
+    # perforce daemon configured to run on ssl
+    it { is_expected.to contain_file('/opt/perforce/.p4config').with_content(/ssl:1666/) }
+  end
 end
