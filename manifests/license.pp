3b19f58cf6b5bb2a7b6760a1070354ed95ba9c73
Mike Marseglia <mike@marseglia.org>
2019-01-03 16:09:31 -0500

    add support for managing the perforce license file

---
+++ manifests/license.pp
@@ -0,0 +1,20 @@
+# =Class perforce::license
+#
+# ==Description
+# Installs perforce license.
+#
+class perforce::license {
+
+  # perforce doesn't require a license and will support up to 5 users without one.
+  # 
+  # installing a license
+  # https://www.perforce.com/perforce/doc.current/manuals/p4sag/Content/P4SAG/DB5-71632.html#License
+	if ($perforce::license_content) {
+    file { "${perforce::service_root}/license" :
+		  owner   => 'root',
+		  group   => 'root',
+		  mode    => '0644',
+		  content => $perforce::license_content,
+	  }
+  }
+}
