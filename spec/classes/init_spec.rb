2e23913510f46a0aa4d042fa7e68a955c2b0e426
Mike Marseglia <mike@marseglia.org>
2019-01-05 19:34:40 -0500

    You see i got this problem, cops don't like me, so i don't like cops.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,16 +1,13 @@
-require File.join(File.dirname(__FILE__),'..','spec_helper').to_s
-
+require File.join(File.dirname(__FILE__), '..', 'spec_helper').to_s
 describe 'perforce' do
-
   context 'default parameters' do
     it { is_expected.to compile }
-
-    it { is_expected.to contain_class('perforce::install')}
-    it { is_expected.to contain_class('perforce::configure')}
-    it { is_expected.to contain_class('perforce::package')}
-    it { is_expected.to contain_class('perforce::repository')}
-    it { is_expected.to contain_class('perforce::license')}
-    it { is_expected.to contain_class('perforce::service')}
+    it { is_expected.to contain_class('perforce::install') }
+    it { is_expected.to contain_class('perforce::configure') }
+    it { is_expected.to contain_class('perforce::package') }
+    it { is_expected.to contain_class('perforce::repository') }
+    it { is_expected.to contain_class('perforce::license') }
+    it { is_expected.to contain_class('perforce::service') }
 
     # perforce's main yum repository
     it { is_expected.to contain_yumrepo('perforce') }
@@ -27,23 +24,23 @@ describe 'perforce' do
   end
 
   context 'with ssl' do
-    let (:params) {{
-        'service_ssldir'   => '/opt/perforce/p4ssldir',
-    }}
+    let(:params) do
+      { 'service_ssldir' => '/opt/perforce/p4ssldir' }
+    end
 
     # perforce ssl directory environment variable is set
-    it { is_expected.to contain_file('/opt/perforce/.p4config').with_content(/^P4SSLDIR/) }
+    it { is_expected.to contain_file('/opt/perforce/.p4config').with_content(%r{^P4SSLDIR}) }
 
     # perforce daemon configured to run on ssl
-    it { is_expected.to contain_file('/opt/perforce/.p4config').with_content(/ssl:1666/) }
+    it { is_expected.to contain_file('/opt/perforce/.p4config').with_content(%r{ssl:1666}) }
   end
 
   context 'with license' do
-	  let (:params) {{
-		  'license_content'	=> 'License:	1234567890ABCDEFGHIJK',
-	  }}
+    let(:params) do
+      { 'license_content'	=> 'License:	1234567890ABCDEFGHIJK' }
+    end
 
-	  # is_expected.to create a license file in the perforce root directory
-	  it { is_expected.to contain_file('/opt/perforce/p4root/license').with_content(/License:	1234567890ABCDEFGHIJK/) }
+    # is_expected.to create a license file in the perforce root directory
+    it { is_expected.to contain_file('/opt/perforce/p4root/license').with_content(%r{License:	1234567890ABCDEFGHIJK}) }
   end
 end
