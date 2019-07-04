e9062758983fa006abed9c5a2ee7deb11607f3a7
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2019-07-04 22:17:27 +0200

    Ensure license file is removed if unused

--- manifests/license.pp
+++ manifests/license.pp
@@ -6,15 +6,18 @@
 class perforce::license {
 
   # perforce doesn't require a license and will support up to 5 users without one.
-  # 
+  #
   # installing a license
   # https://www.perforce.com/perforce/doc.current/manuals/p4sag/Content/P4SAG/DB5-71632.html#License
-  if ($perforce::license_content) {
-    file { "${perforce::service_root}/license" :
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => $perforce::license_content,
-    }
+  file { "${perforce::service_root}/license" :
+    ensure  => if $perforce::license_content {
+      'file'
+    } else {
+      'absent'
+    },
+    owner   => $perforce::user,
+    group   => 'root',
+    mode    => '0400',
+    content => $perforce::license_content,
   }
 }
